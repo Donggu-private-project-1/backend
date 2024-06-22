ab87d4777331d013d1cbb28bee27490a690414e3
DOLONG9 <bagmy2@gmail.com>
2024-06-23 00:09:47 +0900

    springboot upload

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 # 기반 이미지로부터 시작
-FROM nginx:latest
+FROM openjdk:17-jdk
 
-# 컨테이너 내부의 /usr/share/nginx/html 디렉토리로 index.html 복사
-COPY index.html /usr/share/nginx/html/
+WORKDIR /app
 
-# 포트 80을 노출하여 HTTP 트래픽 허용
-EXPOSE 80
+COPY target/*.jar app.jar
 
-# 컨테이너 시작 시 NGINX 실행
-CMD ["nginx", "-g", "daemon off;"]
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ demo/src/main/java/com/donggu/demo/Controller/mainController.java
@@ -0,0 +1,16 @@
+package com.donggu.demo.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mainController {
+
+    @CrossOrigin(origins = {"http://donggu-1-nginx"})
+    @GetMapping("/")
+    public String test() {
+        return "{ name:동구 }";
+    }
+
+}
