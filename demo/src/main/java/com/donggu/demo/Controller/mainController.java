71260fab3252a0c6bcbf89ee8bbfa79434c441b4
DOLONG9 <bagmy2@gmail.com>
2024-06-23 12:46:26 +0900

    test

--- demo/src/main/java/com/donggu/demo/Controller/mainController.java
+++ demo/src/main/java/com/donggu/demo/Controller/mainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class mainController {
 
-    @CrossOrigin(origins = {"http://donggu-1-nginx/test"})
+    @CrossOrigin(origins = {"http://donggu-1-nginx/"})
     @GetMapping("/test")
     public String test() {
         return "{ name:동구 }";
